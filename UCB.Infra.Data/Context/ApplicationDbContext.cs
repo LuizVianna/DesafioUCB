01ad7d8dcb14c2fb34b63873335d55cbb7285140
Luiz Claudio de Almeida Vianna <lcavianna@hotmail.com>
2024-07-19 14:28:34 -0300

    Add project files.

---
+++ UCB.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UCB.Infra.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        //public DbSet<Item> Item { get; set; }
+        //public DbSet<Pedido> Pedido { get; set; }
+        //public DbSet<Usuario> Usuario { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
---
+++ UCB.Infra.IoC/DependencyInjection.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UCB.Infra.Data.Context;
+
+namespace UCB.Infra.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfraStructure(this IServiceCollection service, IConfiguration configuration)
+        {
+            service.AddDbContext<ApplicationDbContext>(options =>
+            {
+                //options.UseInMemoryDatabase(configuration.GetConnectionString("DefaultConnection"),
+                //    b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName));
+            });
+
+            service.AddAuthentication(
+                    opt =>
+                    {
+                        opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                        opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                    }).AddJwtBearer(option =>
+                    {
+                        option.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateIssuer = true,
+                            ValidateAudience = true,
+                            ValidateLifetime = true,
+                            ValidateIssuerSigningKey = true,
+
+                            ValidIssuer = configuration["jwt:issuer"],
+                            ValidAudience = configuration["jwt:audience"],
+                            IssuerSigningKey = new SymmetricSecurityKey(
+                                                Encoding.UTF8.GetBytes(configuration["jwt:secretkey"])),
+                            ClockSkew = TimeSpan.Zero
+                        };
+                    });
+
+
+            //service.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+            //Repositories
+            //service.AddScoped<IPedidoRepository, PedidoRepository>();
+            //service.AddScoped<IItemRepository, ItemRepository>();
+            //service.AddScoped<IUsuarioRepository, UsuarioRepository>();
+
+            //Services
+            //service.AddScoped<IItemService, ItemService>();
+            //service.AddScoped<IPedidoService, PedidoService>();
+            //service.AddScoped<IUsuarioService, UsuarioService>();
+            //service.AddScoped<IAuthenticate, AuthenticateService>();
+
+            return service;
+        }
+    }
+}
